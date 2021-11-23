046b93091710098d1ebe5493b925745c432e48dc
Natanaeleb <natanaelebalic@gmail.com>
2021-11-23 11:32:30 -0300

    comecando API todo

--- todos/views.py
+++ todos/views.py
@@ -1,17 +1,22 @@
 from common.serializers import ErrorSerializer
 from todos.db.services import TodosDBService
 from fastapi import APIRouter
-from .serializers import TodoCreateSerializer, TodoUpdateSerializer
+from .serializers import TodoCreateSerializer, TodoSerializer, TodoUpdateSerializer
 
 todo_router = APIRouter()
 
 '''
 GET todos/
 '''
-
+@todo_router.get('/todos/')
+def get_title():
+    user = TodoCreateSerializer.get_title()
 ''''
 GET todos/{id}/
 '''
+@todo_router.get('/todos/{id}')
+def get_user(id: str):
+    user = TodoSerializer.get_user(id)
 
 '''
 POST todos
