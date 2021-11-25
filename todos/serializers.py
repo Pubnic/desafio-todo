8253e4e9a13ddd30e22ec67dfd53b9aa518af125
AdneMoretti <morettiadne1@gmail.com>
2021-11-25 14:32:02 -0300

    Adicionando mudanças no arquivo view

--- todos/serializers.py
+++ todos/serializers.py
@@ -7,12 +7,17 @@ from datetime import datetime
 class TodoSerializer(BaseModel):
     id: str
 
+
     class Config:
         orm_mode = True
 
 
 class TodoCreateSerializer(BaseModel):
     title: str
+    status: str
+    due_date: datetime
+    responsible: str
 
 class TodoUpdateSerializer(TodoCreateSerializer):
+    
     pass
