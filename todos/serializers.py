f41ba58cbc131bb384b4b880c0066bbe768f2ebe
AdneMoretti <morettiadne1@gmail.com>
2021-11-30 12:31:04 -0300

    Atualizando as rotas do CRUD

--- todos/serializers.py
+++ todos/serializers.py
@@ -1,24 +1,27 @@
 from .enums import Status
 from pydantic import BaseModel
-from datetime import date, datetime
-
+from datetime import datetime
 
 
 class TodoSerializer(BaseModel):
     id: str
     title: str
-
+    description: str
+    status: Status
+    responsible: str
+    due_date: datetime
 
     class Config:
         orm_mode = True
 
 
 class TodoCreateSerializer(BaseModel):
-    title: str 
+    title: str
     description: str
-    status: str
-    responsible:str   
+    status: Status
+    responsible: str
     due_date: datetime
+
+
 class TodoUpdateSerializer(TodoCreateSerializer):
-    
     pass
--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,9 @@
 from common.serializers import ErrorSerializer
+from starlette.responses import JSONResponse
 from todos.db.services import TodosDBService
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, HTTPException
 from .serializers import TodoCreateSerializer, TodoSerializer, TodoUpdateSerializer
-from typing import List 
+
 
 todo_router = APIRouter()
 banco_dados = TodosDBService()
@@ -10,48 +11,60 @@ banco_dados = TodosDBService()
 '''
 GET todos/
 '''
+
+
 @todo_router.get("/")
 def get_todos():
+
     todos = banco_dados.get_todos()
     return todos
+
 
 ''''
 GET todos/{id}/
 '''
-@todo_router.get("/{todo_id}")
-def get_todos_id(todo_id:str):
-    todo = banco_dados.get_todo(todo_id)
-    if todo is None:
-        return {"Error": "Todo not found"}
-    return{"message": f"{todo}"}
+
+
+@todo_router.get("/{todo_id}/", response_model=TodoSerializer)
+def get_todos_id(todo_id: str):
+
+    try:
+        todo = banco_dados.get_todo(todo_id)
+
+        return todo
+    except Exception:
+        return JSONResponse
+
+
 '''
 POST todos
 '''
-@todo_router.post(
-    "/", 
-    status_code=status.HTTP_201_CREATED,
-    response_model=TodoCreateSerializer)
+
+
+@todo_router.post("/", status_code=status.HTTP_201_CREATED)
 def create_todo(todo: TodoCreateSerializer):
-    new_todo = banco_dados.create_todo(todo)
-    return todo
+    return banco_dados.create_todo(todo)
+
 
 '''
 PUT todos
 '''
-@todo_router.put("/{todo_id}/",status_code=status.HTTP_201_CREATED)
-def update_todo(todo_id: str, todo: TodoCreateSerializer):
-    new_todo = banco_dados.update_todo(todo_id, todo)
-    return todo
+
+
+@todo_router.put("/{todo_id}/")
+def update_todo(todo_id: str, todo: TodoUpdateSerializer):
+
+    return banco_dados.update_todo(todo_id, todo)
+
 
 ''''
 DELETE todos/{id}/
 '''
+
+
 @todo_router.delete("/{todo_id}/", status_code=status.HTTP_204_NO_CONTENT)
-def delete_todo(todo_id:str):
-    new_todo = banco_dados.delete_todo(todo_id)
-    if new_todo is None:
+def delete_todo(todo_id: str):
+    deleted = banco_dados.delete_todo(todo_id)
+    if not deleted:
         return {"Error": "Todo not found"}
     return {"Message": "Todo deleted"}
-    print("feito")
-    
-    
