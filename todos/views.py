1e09a8adadd839b157082fb82c555176fd5f34ec
Natanaeleb <natanaelebalic@gmail.com>
2021-12-01 11:20:11 -0300

    corrigindo erros nas rotas (barras)

--- todos/views.py
+++ todos/views.py
@@ -3,7 +3,7 @@ from todos.db.models import TodoModel
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 from .serializers import TodoCreateSerializer, TodoUpdateSerializer
-
+from .serializers import TodoSerializer
 
 todo_router = APIRouter()
 info_database = TodosDBService()
@@ -25,14 +25,14 @@ GET todos/{id}/
 '''
 
 
-@todo_router.get('/id/{id}')
+@todo_router.get('/{id}/', response_model=TodoSerializer)
 def get_todo(id: str):
     try:
         todo_id = info_database.get_todo(id)
     except TodoModel.DoesNotExist:
         return JSONResponse(
             status_code=404,
-            content=dict(error='Todo not found')
+            content=dict(error='Todo not found.')
         )
     return todo_id
 
@@ -53,10 +53,15 @@ PUT todos
 '''
 
 
-@todo_router.put('/id/{todo_id}')
+@todo_router.put('/{todo_id}/')
 def update_todo(todo_id: str, todo: TodoUpdateSerializer):
-    up_todo = info_database.update_todo(todo_id, todo)
-    return up_todo
+    try:
+        return info_database.update_todo(todo_id, todo)
+    except TodoModel.DoesNotExist:
+        return JSONResponse(
+            status_code=404,
+            content=dict(error='Todo not found.')
+        )
 
 
 '''
@@ -64,9 +69,12 @@ DELETE todos/{id}/
 '''
 
 
-@todo_router.delete('/id/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
+@todo_router.delete("/{todo_id}/", status_code=status.HTTP_204_NO_CONTENT)
 def delete_todo(todo_id: str):
-    deleted = info_database.delete_todo(todo_id)
-    if not deleted:
-        return {"Error": "Todo not found"}
-    return {"Message": "Todo deleted"}
+    try:
+        info_database.delete_todo(todo_id)
+    except TodoModel.DoesNotExist:
+        return JSONResponse(
+            status_code=404,
+            content=dict(error='Todo not found.')
+        )
