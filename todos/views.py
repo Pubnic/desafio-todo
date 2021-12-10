666b60cfd7d689ba66665c472bcf765b06bf3bf1
Débora Janini <deborajaninicg@gmail.com>
2021-12-10 15:38:41 -0300

    atualização em views

--- todos/views.py
+++ todos/views.py
@@ -4,10 +4,24 @@ from fastapi import APIRouter
 from .serializers import TodoCreateSerializer, TodoUpdateSerializer
 
 todo_router = APIRouter()
+database = TodosDBService()
 
 '''
 GET todos/
+
+Aqui estou usando a aula e o git das meninas como base,peguei a lógica
+do primeiro get no app
 '''
+
+@todo_router.get('/') #MÉTODO GET
+def get_todos(): #DEFINIÇÃO DA FUNÇÃO PEGAR_TODOS
+    alltodos = database.get_todos() #DECLARAÇÃO DE VARIÁVEL TODOS OS TODOS 
+    #RECEBE DO BANCO DE DADOS GET_TODOS
+    return alltodos
+
+    #POSSO FAZER DESSA FORMA:
+    #RETURN database.get_todos()
+
 
 ''''
 GET todos/{id}/
